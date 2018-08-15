85082ee5911a489f95054cc6d1cfdb2ac1bd2edc
Adil Abbas <cyberphilomath@gmail.com>
2018-08-15 22:38:17 +0600

    Initial Commit

---
+++ index.py
@@ -0,0 +1,17 @@
+# author Adil Abbas
+# ----------------------------------
+#       Web Scraping
+# ----------------------------------
+
+
+from webscrap import wlog
+from webscrap import wscrap
+
+wlog.set_custom_log_info('html/error.log')
+
+news_scrap = wscrap.NewsScraper(wscrap.url_aj, wlog)
+
+news_scrap.read_webpage_from_html()
+news_scrap.convert_data_to_bs4()
+
+news_scrap.parse_soup_to_simple_html()
